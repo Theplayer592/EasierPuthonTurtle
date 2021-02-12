32ce45990c01388127d4fde966e79092db60e3b9
Theplayer592 <64487304+Theplayer592@users.noreply.github.com>
2021-02-12 08:25:06 +0000

    Create main.py
    This is where you add all of the actual code for the turtle.

---
+++ main.py
@@ -0,0 +1,7 @@
+#This is the source file where you write your code
+
+import EasierPythonTurtle #Import the library
+
+t = EasierPythonTurtle #Create a new turtle that is compatible with the library
+
+t.drawShape() #Create a square
